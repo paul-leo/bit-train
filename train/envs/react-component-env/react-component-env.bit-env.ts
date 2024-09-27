b52a3e947f758255a093243cc6e106ed1bed5720
roc <roc.liu@hotmail.com>
2024-09-27 14:30:05 +0800

    优化

--- train/envs/react-component-env/react-component-env.bit-env.ts
+++ train/envs/react-component-env/react-component-env.bit-env.ts
@@ -1,16 +1,18 @@
 /**
-* This env uses bitdev.react/react-env, inspect it's config and API - https://bit.cloud/bitdev/react/react-env
-* Learn more on how you can customize your env here - https://bit.dev/docs/react-env/set-up-your-env
-*/
+ * This env uses bitdev.react/react-env, inspect it's config and API - https://bit.cloud/bitdev/react/react-env
+ * Learn more on how you can customize your env here - https://bit.dev/docs/react-env/set-up-your-env
+ */
 import { ReactEnv } from '@bitdev/react.react-env';
 import { createRequire } from 'node:module';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { Preview } from '@teambit/preview';
+import { EnvHandler } from '@teambit/envs';
+import { ReactPreview } from '@teambit/preview.react-preview';
 
 const require = createRequire(import.meta.url);
 
 export class ReactComponentEnv extends ReactEnv {
-
   /* a shorthand name for the env */
   name = 'react-component-env';
 
@@ -23,7 +25,9 @@ export class ReactComponentEnv extends ReactEnv {
   protected eslintConfigPath = require.resolve('./config/eslintrc.cjs');
 
   /* Prettier config. Learn how to replace formatter - https://bit.dev/reference/formatting/set-up-formatter */
-  protected prettierConfigPath = require.resolve('./config/prettier.config.cjs');
+  protected prettierConfigPath = require.resolve(
+    './config/prettier.config.cjs'
+  );
 
   /* Vitest config. Learn how to replace tester - https://bit.dev/reference/testing/set-up-tester */
   protected vitestConfigPath = require.resolve('./config/vitest.config.mjs');
@@ -32,6 +36,16 @@ export class ReactComponentEnv extends ReactEnv {
   protected previewMounter = require.resolve('./preview/mounter.js');
 
   protected dirName = dirname(fileURLToPath(import.meta.url));
+
+  preview(): EnvHandler<Preview> {
+    /**
+     * @see https://bit.dev/docs/react-env/component-previews
+     */
+    return ReactPreview.from({
+      mounter: require.resolve('./preview/mounter'),
+      // transformers: [webpackTransformer],
+    });
+  }
 }
 
 export default new ReactComponentEnv();
